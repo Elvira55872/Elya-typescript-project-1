5322d803e2a0bc0653b0121e038669ce338a3fc8
“Elvira55872” <elli-x1996@mail.ru>
2024-07-19 21:22:49 +0200

    Layout work in progress

--- src/pages/EmployeeAppProject/Layout_Team_1/styles.ts
+++ src/pages/EmployeeAppProject/Layout_Team_1/styles.ts
@@ -1,12 +1,18 @@
 import styled from "@emotion/styled";
 import { NavLink, Link as SimpleLink } from "react-router-dom";
 
-import { colors } from "styles/colors";
+export enum COLORS_TEAM_1 {
+  BACKGROUND_BLUE = "#112233",
+  HEADER_WHITE = "#FAF9FF",
+  CARD_WHITE = "#FFFFFF",
+}
 
 export const LayoutWrapper = styled.div`
   display: flex;
   flex-direction: column;
   flex: 1;
+  background-color: ${COLORS_TEAM_1.BACKGROUND_BLUE};
+  font-family: Arial, Helvetica, sans-serif;
 `;
 
 export const Header = styled.header`
@@ -14,8 +20,8 @@ export const Header = styled.header`
   justify-content: space-between;
   width: 100%;
   height: 100px;
-  border-bottom: 1px solid black;
-  background-color: ${colors.PRIMARY};
+  /* border-bottom: 1px solid black; */
+  background-color: ${COLORS_TEAM_1.HEADER_WHITE};
   padding: 20px 40px;
   color: white;
 `;
@@ -49,33 +55,4 @@ export const Main = styled.main`
   display: flex;
   flex: 1;
   padding: 40px;
-`;
-
-export const Footer = styled.footer`
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-  width: 100%;
-  height: 150px;
-  border-top: 1px solid black;
-  background-color: ${colors.PRIMARY};
-  padding: 20px 40px;
-  color: white;
-`;
-
-export const FooterLogo = styled.div`
-  height: 80px;
-  width: 80px;
-`;
-
-export const FooterNavigation = styled.nav`
-  display: flex;
-  flex-direction: column;
-  gap: 10px;
-`;
-
-export const FooterLink = styled(SimpleLink)`
-  color: white;
-  font-size: 14px;
-  font-weight: bold;
 `;
