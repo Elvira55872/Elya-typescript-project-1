3f70bc003e6b7c5aa7a13eae02454ffd27fc9116
SergeiVS <fam.smirnoff@gmail.com>
2024-07-20 21:16:46 +0200

    merge changes

--- src/pages/EmployeeAppProject/Layout_Team_1/styles.ts
+++ src/pages/EmployeeAppProject/Layout_Team_1/styles.ts
@@ -46,7 +46,6 @@ export const NavigationContainer = styled.nav`
 `;
 
 export const Link = styled(NavLink)`
-//Прошу прощения, что влез сюда но белое на белом не было видно.
   color: black;
   font-size: 20px;
   font-weight: normal;
